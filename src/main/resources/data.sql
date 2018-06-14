1a3dacbc08eb807ba54e2fdc76e6a5c81e7c73cb
rohit jain <rohit1987j@gmail.com>
2018-06-15 00:18:10 +0200

    adding data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(id, name) values (1, 'rohit');
+insert into course(id, name) values (1, 'java');
+insert into passport(id, no ) values (1, 'E1234');
+insert into review(id, description, rating ) values (1, 'very good', 5);
