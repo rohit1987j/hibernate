46fab8f58038693ee2ee2de1261f39daf588557c
rohit jain <rohit1987j@gmail.com>
2018-06-24 21:06:53 +0200

    moving to jpa repository

--- src/main/java/com/example/hibernate/repository/PassportRepository.java
+++ src/main/java/com/example/hibernate/repository/PassportRepository.java
@@ -2,17 +2,14 @@ package com.example.hibernate.repository;
 
 import com.example.hibernate.entity.Passport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Id;
 
 @Repository
 @Transactional
-public class PassportRepository {
-    @Autowired
-    private EntityManager entityManager;
-    public Passport findById(int id) {
-        return entityManager.find(Passport.class, id);
-    }
+public interface PassportRepository extends JpaRepository<Passport, Id> {
 }
