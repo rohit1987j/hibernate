fbe9df1cfee80e5b52db84953497b08ce938723a
r.jain <rohit1987j@gmail.com>
2018-07-12 18:22:27 +0200

    adding role and user

---
+++ src/main/java/com/example/hibernate/entity/Role.java
@@ -0,0 +1,50 @@
+package com.example.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
+	private int id;
+
+	private String name;
+
+	@ManyToMany(mappedBy = "roles")
+	private List<User> user = new ArrayList<>();
+
+	public List<User> getUser() {
+		return user;
+	}
+
+	public void addUser(User user) {
+		this.user.add(user);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/com/example/hibernate/entity/User.java
@@ -0,0 +1,85 @@
+package com.example.hibernate.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private int id;
+
+	private String name;
+
+	private String email;
+
+	private String password;
+
+	@Column(name = "is_active")
+	private boolean isActive;
+
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "role_id")
+	private List<Role> roles = new ArrayList<>();
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Role role) {
+		this.roles.add(role);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean active) {
+		isActive = active;
+	}
+}
